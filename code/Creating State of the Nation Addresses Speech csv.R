3753c14d2b75230e2d8b19d0798b48e5b9a50e9a
Amanda McDermott <42814674+Glacieus@users.noreply.github.com>
2018-11-05 16:02:49 -0500

    Update code/Creating State of the Nation Addresses Speech csv.R

--- code/Creating State of the Nation Addresses Speech csv.R
+++ code/Creating State of the Nation Addresses Speech csv.R
@@ -23,8 +23,13 @@ sona_dates <- parse_date(sona_tb$value, "%B %d, %Y")
 
 # Parsing missed a date so manually entering it in
 sona_dates[328] <- as.Date("2016-07-25")
+
+#Convert to a dataframe; separate year from month and day; select only the year column
+sona_dates <- as.data.frame(sona_dates)
 sona_dates <- sona_dates %>% 
-  na.omit()
+  na.omit() %>% 
+  separate(sona_dates, into = c("year", "month", "day"), sep = "-", convert = F) %>%
+  select(year)
 
 # Get Unique HTML Links ---------------------------------------------------
 
@@ -47,10 +52,32 @@ scrape_it <- function(url){
 }
 
 # Test - Incomplete 
-t1 <- urls[1:3]
-
 for(i in 1:length(urls)){
   test_test <- scrape_it(urls[i])
-  indic<- as.character(i + 1934)
-  write.csv(test_test, paste("philippines_", indic, ".txt", sep = ""))
+  #indic<- as.character(i + 1934)
+  write.csv(test_test, paste("philippines_", ".txt", sep = ""))
 }
+
+###Reading in the files
+files = list.files(pattern="*.txt")
+fls <- NULL
+lns <- NULL
+for (file in files) {
+  my_lines <- readLines(file)
+  print(file)
+  for (line in my_lines) {
+    fls <- c(fls, file)
+    lns <- c(lns, line)
+  }
+}
+
+###Dataframe stuff.
+df <- data.frame(file=fls, text =lns)
+new_df <- df %>% 
+  separate(file, into = c("country", "delete_me", "year"), sep = "_") 
+newer_df <- new_df %>% 
+  mutate(year = gsub(".txt", "", year))
+newer_df$delete_me <-NULL 
+newer_df$context <- "UNGD"
+
+write_csv(newer_df, "USUNGD.csv")
